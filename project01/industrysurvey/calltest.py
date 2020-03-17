38ddd81bf4a45fce991e62eb2f278d0457a247a3
mt-hara <mthara3@gmail.com>
2020-03-17 16:32:12 +0900

    commit from 8300 2020-03-17 16:30

--- project01/industrysurvey/calltest.py
+++ project01/industrysurvey/calltest.py
@@ -1,2 +1,12 @@
+import typing
+import dataclasses
 
+@dataclasses.dataclass
+class MyConfig:
+    foo : int
+    bar : float
+    baz : typing.List[str]
 
+config = MyConfig(foo = 1, bar=2.0, baz = ["3 and 4"])
+
+print(config.foo)
--- project01/industrysurvey/dto/exceldata.py
+++ project01/industrysurvey/dto/exceldata.py
@@ -1,9 +1,39 @@
 from dto.excelbase import AbsExcelApp
 from dto.excelbase import AbsExcelWorkBook
+import dataclasses
 
 class ExcelData():
     def __init__(self, app, filepath):
-        self.xlwb = AbsExcelWorkBook(app, filepath)
+        self.wb = AbsExcelWorkBook(app, filepath)
+        self.ws = self.wb.xlws
+
+@dataclasses.dataclass
+class CustomerMaster:
+    customercd : str
+    ringino : str
+    customername : str
+    CustomerShortName : str
+    excludeLaw : bool
+    headOfficeZipCd : str
+    headOfficeAddress1 : str
+    headOfficeAddress2 : str
+    headOfficeTel : str
+    headOfficeFax : str
+    BranchOfficeZipCd : str
+    BranchOfficeAddress1 : str
+    BranchOfficeAddress2 : str
+    BranchOfficeTel : str
+    BranchOfficeFax : str
+    repName : str
+    repKanaName : str
+    repJobTitle : str
+    repBirthday :str
 
 
-
+if __name__ == "__main__":
+    xlapp = AbsExcelApp()
+    filename = "C:\\Users\\m-hara\\Desktop\\取引先コード取得済\\業態調査票（（株）清和光学製作所）.xlsx"
+    xlbook = ExcelData(xlapp.app, filename)
+    var = xlbook.ws.name
+    print(var)
+    xlapp.close_app()

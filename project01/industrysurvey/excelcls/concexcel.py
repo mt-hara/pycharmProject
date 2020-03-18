015c440bf6c37b22b9417367fd09d97c7c373ca2
mt-hara <mthara3@gmail.com>
2020-03-18 14:57:11 +0900

    commit from 8300 2020-03-18 14:57

--- project01/industrysurvey/excelcls/concexcel.py
+++ project01/industrysurvey/excelcls/concexcel.py
@@ -1,3 +1,5 @@
+from typing import List, Any
+
 from excelcls.absexcel import AbsExcelApp
 from excelcls.absexcel import AbsExcelWorkBook
 
@@ -13,10 +15,37 @@ class ConcExcelApp(AbsExcelApp):
 
 class ConcExcelWorkBook():
 
+    sh_pos: List[Any]
+
     def __init__(self, app, filepath):
         self.wb = AbsExcelWorkBook(app, filepath)
         self.ws = self.wb.xlws
-        self.datadict = {}
+        self.shape = self.ws.shapes
+        self.sh_pos = []
 
     def wb_close(self):
         self.wb.close_book()
+
+    def shapes_pos(self):
+        for sh in self.shape:
+            var = {}
+            var["top"] = str(sh.top)
+            var["left"] = str(sh.left)
+            self.sh_pos.append(var)
+            # print("top : " + str(sh.top))
+            # print("left: " + str(sh.left))
+
+
+if __name__ == "__main__":
+    xlapp = ConcExcelApp()
+    filename = "C:\\Users\\m-hara\\Desktop\\取引先コード取得済\\業態調査票（㈱八木熊）.xlsx"
+
+    cli = ConcExcelWorkBook(xlapp.app, filename)
+    cli.shapes_pos()
+
+
+    for i in cli.sh_pos:
+        print(i)
+
+    cli.wb_close()
+    xlapp.close_app()

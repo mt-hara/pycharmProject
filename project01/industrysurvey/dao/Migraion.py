563c0a13d32da7e9eadcbd1e83114d4d7dd405da
mt-hara <mthara3@gmail.com>
2020-04-07 14:58:35 +0900

    commit from 8300 2020-04-07 14:33

--- project01/industrysurvey/dao/Migraion.py
+++ project01/industrysurvey/dao/Migraion.py
@@ -1,15 +1,17 @@
-# from dao.BaseEngine import BaseEngine
+from dao.BaseEngine import BaseEngine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.orm import relation, relationship
 from dao.CreateTable import AllCustomerMaster, StockStatusMaster,Base
 from sqlalchemy.orm.session import sessionmaker
 from sqlalchemy.engine import  create_engine
-# class Migration():
-#     def __init__(self):
-#         self.e = BaseEngine().engine
-#
-#     def AllCustomerMaster(self):
-#         Base().metadata.create_all(self.e)
+class Migration():
+    def __init__(self):
+        self.e = BaseEngine().engine
 
-class User():
-    pass
+    def AllCustomerMaster(self):
+        Base().metadata.create_all(self.e)
+
+
 
 
---
+++ project01/industrysurvey/dao/Relation.py
@@ -0,0 +1,50 @@
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.schema import Column, ForeignKey
+from sqlalchemy.types import Integer, String
+from sqlalchemy.orm import relation, relationship
+from sqlalchemy.engine import create_engine
+from sqlalchemy.orm.session import sessionmaker
+
+Base = declarative_base()
+
+
+class User(Base):
+    __tablename__ = 'users'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String, nullable=False)
+
+    address = relationship('Address', order_by='Address.id',
+                       uselist=False, backref='users')
+    # testtext = relationship("Address", order_by="Address.id",uselist=False, backref="users")
+
+    def __repr__(self):
+        return '<User(%d, %s)>' % (self.id, self.name)
+
+
+class Address(Base):
+    __tablename__ = 'addresses'
+
+    id = Column(Integer,ForeignKey('users.id'), primary_key=True)
+    email = Column(String, nullable=False)
+    # owner_id = Column(Integer, ForeignKey('users.id'))
+
+    def __repr__(self):
+        return '<Address(%d, %s)>' % (self.id, self.email)
+
+
+if __name__ == '__main__':
+    engine = create_engine('sqlite:///' + "C:\\Dev\\DB\\sqlite3\\BizSurvey.sqlite3", echo=True)
+    Base.metadata.create_all(engine)
+    SessionMaker = sessionmaker(bind=engine)
+    session = SessionMaker()
+
+    new_user = User(name='foo')
+    new_user.address = Address(email='foo@example.jp')
+
+    session.add(new_user)
+    session.commit()
+
+    selected_user = session.query(User).first()
+    print(selected_user)
+    print(selected_user.address)

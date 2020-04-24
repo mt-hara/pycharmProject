41d27b5fa4d2e0d69dab37f99698117d93c57666
mt-hara <mthara3@gmail.com>
2020-04-24 16:39:49 +0900

    commit from 8300 2020-04-24 16:04

--- project01/industrysurvey/mainmodule/ExcelDataToSQL.py
+++ project01/industrysurvey/mainmodule/ExcelDataToSQL.py
@@ -2,8 +2,8 @@ import sys
 import traceback
 import pathlib
 import shutil
-
-from mainmodule.FileFunction import FilePicker, ExcelFile
+from selectfiledir.filepicker import GetFile
+from excelapp.ExcelApp import ExcelApp, ExcelWorkBook
 from iterators.ItemIterator import *
 from dto.ExcelSheetDTO import ExcelSheetDTO
 from mainmodule.CreateDTO import ExcelDataToDTO
@@ -16,47 +16,8 @@ from functions.StopWatch import stop_watch
 # log = logging.getLogger(__name__)
 
 # ログデータ保存先ディレクトリパス
+
 _log_dir = "C:\\workspace\\pycharmProject\\project01\\industrysurvey\\LogData"
-
-
-class FilePathList():
-    def __init__(self):
-        self.file_list = []
-        self.file_shelf = ItemShelf()
-        self.pick_file()
-        self.get_file_itr()
-
-    def pick_file(self):
-        picker = FilePicker()
-        picked_list = picker.get_file_list()
-        if picked_list is False:
-            quit()
-        else:
-            self.file_list = picked_list
-            return self.file_list
-
-    def get_file_itr(self):
-        if len(self.file_list) == 0:
-            quit()
-        for files in self.file_list:
-            self.file_shelf.append(files)
-
-        return self.file_shelf
-
-
-class ChangeDir():
-
-    def __init__(self, filepath):
-        self.__NEW_DIR_NAME = "import済"
-        self.filepath = filepath
-        self.__current_dir = pathlib.Path(self.filepath).parent
-        self.__new_dir = self.__current_dir / self.__NEW_DIR_NAME
-
-    def create_dir(self):
-        pathlib.Path(self.__new_dir).mkdir(exist_ok=True)
-
-    def move_file(self):
-        shutil.move(self.filepath, self.__new_dir)
 
 
 def write_log_data(logfile_path, log_data):
@@ -64,55 +25,85 @@ def write_log_data(logfile_path, log_data):
         f.write("\n{}".format(log_data))
 
 
-class ExcelDataToSQL():
+class FilePicker():
     def __init__(self):
-        self.files_inst = FilePathList()
-        self.__get_fileitr()
-        self.full_filepath = ""
-        self.base_name = ""
-        self.excelapp = ExcelFile()
-        self.wb = self.excelapp.wb
+        self.__def_dir = "DeskTop"
+        self.__ftype = [("Excel2003ファイル", "*.xlsx")]
+        self.picker = GetFile(self.__def_dir, self.__ftype)
+        self.file_list = self.picker.get_files()
+
+    def get_file_list(self):
+        if self.file_list is False:
+            return False
+        else:
+            return self.file_list
+
+
+class ExcelFile():
+    def __init__(self):
+        self.base_app = ExcelApp()
+        self.app = self.base_app.app
+        self.wb = ExcelWorkBook()
         self.ws = None
 
-    def __get_fileitr(self):
-        self.file_itr = self.files_inst.file_shelf
+    def open_workbook(self, filepath):
+        self.wb.open_wb(self.app, filepath)
+        self.ws = self.wb.xlws
 
-    def open_wb(self):
-        __log_data = ""
-        _log_file = pathlib.Path(_log_dir) / "execute_log.log"
-        item = self.file_itr.iterator()
-        while item.hasnext():
-            files = item.next()
-            self.full_filepath = files
-            self.base_name = pathlib.Path(files).name
-            try:
-                self.ws = self.excelapp.open_workbook(files)
-            except Exception as e:
-                __log_data = "Error OpenFile:{}".format(self.base_name)
-            else:
-                __log_data = "Success OpenFile:{}".format(self.base_name)
-                self.get_dto()
-            finally:
-                write_log_data(_log_file, __log_data)
+    def close_workbook(self):
+        self.wb.close_workbook()
 
-    def get_dto(self):
-        pass
+    def close_baseapp(self):
+        self.base_app.close_App()
+
+    def close(self):
+        self.close_workbook()
+        self.close_baseapp()
+
+
+class ExcelDataToSQL():
+    def __init__(self):
+        self.file_shelf = ItemShelf()
+        self.filepath = ""
+        self.excelapp = ExcelFile()
+        self.basename = ""
+        self.customercd = ""
+        self.customername = ""
+        self.dto = None
+        self.ws = None
+
+    def close_wb(self):
+        self.excelapp.close_workbook()
+
+    def close_baseapp(self):
+        self.excelapp.close_baseapp()
+
+    def get_file_itr(self):
+        picker = FilePicker()
+        file_list = picker.get_file_list()
+        if file_list is False:
+            self.close_baseapp()
+            quit()
+        else:
+            for item in file_list:
+                self.file_shelf.append(item)
+            return self.file_shelf
+
+    def open_ws(self,filepath):
+        self.basename = pathlib.Path(filepath).name
+        self.filepath = filepath
+
+
 
 
 @stop_watch
 def main():
-    filedata = ExcelDataToSQL()
-    filedata.open_wb()
-    # files = FilePathList()
-    # it = files.file_shelf.iterator()
-    # while it.hasnext():
-    #     item = it.next()
-    #     print(item)
+    todto = ExcelDataToSQL()
+    todto.get_file_itr()
 
-    # ex = ExcelDataToSQL(listmaker.file_list)
-    # lists = ex.file_list
-    # for i in lists:
-    #     print(i)
+    # filedata = ExcelDataToSQL()
+    # filedata.open_wb()
+
 
 
 if __name__ == "__main__":
